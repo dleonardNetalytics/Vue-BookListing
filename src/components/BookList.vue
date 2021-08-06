a76bc3cd2390e8f9668ac9f1639eb7d121d1f6de
William Leonard <dleonard@netalytics.com>
2021-08-06 19:49:33 -0400

    test

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,6 +2,10 @@
 
 <div>
     <h1>{{title}}</h1>
+
+    <ul>
+        <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
 </div>
 
 </template>
@@ -13,7 +17,12 @@
         data ()
         {
             return {
-                title:'All Books'
+                title:'All Books',
+                books:[
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
             }
         }
     }
@@ -23,5 +32,15 @@
 <style >
 h1, h2{
    font-weight: normal;
+}
+
+ul{
+    list-style-type:none;
+    padding: 0;
+}
+
+li{
+    display: block;
+    margin: 0 10px;
 }
 </style>
