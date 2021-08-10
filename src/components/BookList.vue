336025041c25f3f2a8a380112e6df30ab62f1ae7
William Leonard <dleonard@netalytics.com>
2021-08-09 20:01:17 -0400

    test

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -6,6 +6,8 @@
     <ul>
         <book-item v-for="book in books" :key="book.title" :book='book'></book-item>
     </ul>
+    <hr/>
+    <book-form @addBook='appendBook'></book-form>
 </div>
 
 </template>
@@ -13,9 +15,15 @@
 <script>
 
 import BookItem from './BookItem';
+import BookForm from './BookForm'
     export default {
         name: 'BookList',
-        components:{BookItem},
+        components:{BookItem, BookForm},
+        methods:{
+            appendBook (bookTitle,bookAuthor){
+            this.books.push({ title: bookTitle, author: bookAuthor })
+        }
+        },
         data ()
         {
             return {
